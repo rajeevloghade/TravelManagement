9fa651858fc0056b23a3d522a57ef706ffc3ece2
Charu Soni <charumaba@gmail.com>
2021-04-26 14:01:10 +0530

    Package Entity Created

---
+++ src/main/java/com/tms/entity/Package.java
@@ -0,0 +1,82 @@
+package com.tms.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Package")
+public class Package {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int packageId;
+	String packageName;
+	String packageDescription;
+	String packageType;
+	double packageCost;
+	String payment;
+	public Package() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Package(int packageId, String packageName, String packageDescription, String packageType, double packageCost,
+			String payment) {
+		super();
+		this.packageId = packageId;
+		this.packageName = packageName;
+		this.packageDescription = packageDescription;
+		this.packageType = packageType;
+		this.packageCost = packageCost;
+		this.payment = payment;
+	}
+	public Package(String packageName, String packageDescription, String packageType, double packageCost,
+			String payment) {
+		super();
+		this.packageName = packageName;
+		this.packageDescription = packageDescription;
+		this.packageType = packageType;
+		this.packageCost = packageCost;
+		this.payment = payment;
+	}
+	public int getPackageId() {
+		return packageId;
+	}
+	public void setPackageId(int packageId) {
+		this.packageId = packageId;
+	}
+	public String getPackageName() {
+		return packageName;
+	}
+	public void setPackageName(String packageName) {
+		this.packageName = packageName;
+	}
+	public String getPackageDescription() {
+		return packageDescription;
+	}
+	public void setPackageDescription(String packageDescription) {
+		this.packageDescription = packageDescription;
+	}
+	public String getPackageType() {
+		return packageType;
+	}
+	public void setPackageType(String packageType) {
+		this.packageType = packageType;
+	}
+	public double getPackageCost() {
+		return packageCost;
+	}
+	public void setPackageCost(double packageCost) {
+		this.packageCost = packageCost;
+	}
+	public String getPayment() {
+		return payment;
+	}
+	public void setPayment(String payment) {
+		this.payment = payment;
+	}
+	@Override
+	public String toString() {
+		return "Package [packageId=" + packageId + ", packageName=" + packageName + ", packageDescription="
+				+ packageDescription + ", packageType=" + packageType + ", packageCost=" + packageCost + ", payment="
+				+ payment + "]";
+	}
+	
+}
