e616ba97235a28f4f3510aed4d2beac9810e6f27
Rajeev Loghade <rajeevloghade010@gmail.com>
2021-04-26 11:45:31 +0530

    project deployment

---
+++ src/test/java/com/tms/TravelManagement/TravelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.TravelManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
